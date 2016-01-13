cd7cc6b866a4757d43072dc730f519a10aec0148
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-01-14 00:48:55 +0530

    Adding Games cient to Multi Player Activity

--- app/src/main/java/com/asb/goldtrap/MultiPlayerActivity.java
+++ app/src/main/java/com/asb/goldtrap/MultiPlayerActivity.java
@@ -1,19 +1,68 @@
 package com.asb.goldtrap;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.asb.goldtrap.fragments.multiplayer.MultiPlayerMenuFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.turnbased.OnTurnBasedMatchUpdateReceivedListener;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatch;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
+import com.google.android.gms.plus.Plus;
+import com.google.example.games.basegameutils.BaseGameUtils;
 
 public class MultiPlayerActivity extends AppCompatActivity
-        implements MultiPlayerMenuFragment.OnFragmentInteractionListener {
+        implements MultiPlayerMenuFragment.OnFragmentInteractionListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        OnInvitationReceivedListener, OnTurnBasedMatchUpdateReceivedListener {
+
+    public static final String TAG = MultiPlayerActivity.class.getSimpleName();
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mResolvingConnectionFailure = false;
+
+    private boolean mSignInClicked = false;
+
+    private boolean mAutoStartSignInFlow = true;
+
+    private TurnBasedMatch mTurnBasedMatch;
+
+    // For our intents
+    private static final int RC_SIGN_IN = 9001;
+    final static int RC_SELECT_PLAYERS = 10000;
+    final static int RC_LOOK_AT_MATCHES = 10001;
+
+    public boolean isDoingTurn = false;
+
+    public TurnBasedMatch mMatch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player);
+        // Create the Google API Client with access to Plus and Games
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, MultiPlayerMenuFragment.newInstance(),
                         MultiPlayerMenuFragment.TAG)
@@ -37,17 +86,148 @@ public class MultiPlayerActivity extends AppCompatActivity
     }
 
     @Override
-    public void onAutoMatch() {
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart(): Connecting to Google APIs");
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop(): Disconnecting from Google APIs");
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onAutoMatch() {
+        Bundle autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                1, 1, 0);
+
+        TurnBasedMatchConfig tbmc = TurnBasedMatchConfig.builder()
+                .setAutoMatchCriteria(autoMatchCriteria).build();
+
+        // Start the match
+        ResultCallback<TurnBasedMultiplayer.InitiateMatchResult> cb =
+                new ResultCallback<TurnBasedMultiplayer.InitiateMatchResult>() {
+                    @Override
+                    public void onResult(TurnBasedMultiplayer.InitiateMatchResult result) {
+
+                    }
+                };
+        Games.TurnBasedMultiplayer.createMatch(mGoogleApiClient, tbmc).setResultCallback(cb);
     }
 
     @Override
     public void onStartMatch() {
-
+        Intent intent = Games.TurnBasedMultiplayer.getSelectOpponentsIntent(mGoogleApiClient,
+                1, 1, true);
+        startActivityForResult(intent, RC_SELECT_PLAYERS);
     }
 
     @Override
     public void onGamesInProgress() {
+        Intent intent = Games.TurnBasedMultiplayer.getInboxIntent(mGoogleApiClient);
+        startActivityForResult(intent, RC_LOOK_AT_MATCHES);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "onConnected(): Connection successful");
+
+        // Retrieve the TurnBasedMatch from the connectionHint
+        if (connectionHint != null) {
+            mTurnBasedMatch = connectionHint.getParcelable(Multiplayer.EXTRA_TURN_BASED_MATCH);
+
+            if (mTurnBasedMatch != null) {
+                if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()) {
+                    Log.d(TAG, "Warning: accessing TurnBasedMatch when not connected");
+                }
+
+                updateMatch(mTurnBasedMatch);
+
+            }
+        }
+        Games.Invitations.registerInvitationListener(mGoogleApiClient, this);
+        Games.TurnBasedMultiplayer.registerMatchUpdateListener(mGoogleApiClient, this);
+    }
+
+    private void updateMatch(TurnBasedMatch match) {
+        mMatch = match;
+
+        int status = match.getStatus();
+        int turnStatus = match.getTurnStatus();
+
+        switch (status) {
+            case TurnBasedMatch.MATCH_STATUS_CANCELED:
+                showMessage("This game was canceled!");
+                break;
+            case TurnBasedMatch.MATCH_STATUS_EXPIRED:
+                showMessage("This game is expired.  So sad!");
+                break;
+            case TurnBasedMatch.MATCH_STATUS_AUTO_MATCHING:
+                showMessage("We're still waiting for an automatch partner.");
+                break;
+            case TurnBasedMatch.MATCH_STATUS_COMPLETE:
+                if (turnStatus == TurnBasedMatch.MATCH_TURN_STATUS_COMPLETE) {
+                    showMessage(
+                            "This game is over; someone finished it, and so did you!  There is nothing to be done.");
+                    break;
+                }
+        }
+    }
+
+    private void showMessage(String msg) {
+        ViewGroup coordinateLayout = (ViewGroup) findViewById(R.id.fragment_container);
+        Snackbar.make(coordinateLayout, msg, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended():  Trying to reconnect.");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed(): attempting to resolve");
+        if (mResolvingConnectionFailure) {
+            // Already resolving
+            Log.d(TAG, "onConnectionFailed(): ignoring connection failure, already resolving.");
+            return;
+        }
+
+        // Launch the sign-in flow if the button was clicked or if auto sign-in is enabled
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+
+            mResolvingConnectionFailure = BaseGameUtils.resolveConnectionFailure(this,
+                    mGoogleApiClient, connectionResult, RC_SIGN_IN,
+                    getString(R.string.signin_other_error));
+        }
+
+    }
+
+    @Override
+    public void onInvitationReceived(Invitation invitation) {
+
+    }
+
+    @Override
+    public void onInvitationRemoved(String s) {
+
+    }
+
+    @Override
+    public void onTurnBasedMatchReceived(TurnBasedMatch turnBasedMatch) {
+
+    }
+
+    @Override
+    public void onTurnBasedMatchRemoved(String s) {
 
     }
 }
