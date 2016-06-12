89a844062191027df6415fef64e68241de393f4c
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-06-12 14:17:11 +0530

    Separate Purchase part to a separate activity.

---
+++ app/src/main/java/com/asb/goldtrap/AbstractPurchaseActivity.java
@@ -0,0 +1,132 @@
+package com.asb.goldtrap;
+
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.asb.goldtrap.iap.util.IabBroadcastReceiver;
+import com.asb.goldtrap.iap.util.IabHelper;
+import com.asb.goldtrap.iap.util.IabResult;
+import com.asb.goldtrap.iap.util.Inventory;
+import com.asb.goldtrap.iap.util.Key;
+import com.asb.goldtrap.iap.util.Purchase;
+import com.asb.goldtrap.models.buyables.BuyableType;
+import com.asb.goldtrap.models.iap.IAPCreditor;
+import com.asb.goldtrap.models.iap.factory.IAPCreditorFactory;
+
+/**
+ * Abstract Purchase Activity.
+ * Created by arjun on 12/06/16.
+ */
+public abstract class AbstractPurchaseActivity extends AppCompatActivity
+        implements IabBroadcastReceiver.IabBroadcastListener {
+    private static final String TAG = AbstractPurchaseActivity.class.getSimpleName();
+    protected IabHelper iabHelper;
+    protected IabBroadcastReceiver mBroadcastReceiver;
+    protected IAPCreditorFactory iapCreditorFactory;
+    protected String[] consumables;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        iapCreditorFactory = new IAPCreditorFactory();
+        consumables = getResources().getStringArray(R.array.iap_sku);
+        setupIAP();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mBroadcastReceiver != null) {
+            unregisterReceiver(mBroadcastReceiver);
+        }
+        Log.d(TAG, "Destroying helper.");
+        if (iabHelper != null) {
+            iabHelper.disposeWhenFinished();
+            iabHelper = null;
+        }
+    }
+
+    private void setupIAP() {
+        iabHelper = new IabHelper(this, Key.value());
+        iabHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+                if (null != iabHelper && result.isSuccess()) {
+                    mBroadcastReceiver = new IabBroadcastReceiver(AbstractPurchaseActivity.this);
+                    IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
+                    registerReceiver(mBroadcastReceiver, broadcastFilter);
+                    loadMyInventory();
+                }
+            }
+        });
+    }
+
+    protected void handleInAppPurchase(String sku, int requestCode, String developerPayload) {
+        try {
+            if (null != iabHelper) iabHelper.flagEndAsync();
+            iabHelper.launchPurchaseFlow(this, sku, requestCode,
+                    new IabHelper.OnIabPurchaseFinishedListener() {
+                        @Override
+                        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+                            if (result.isSuccess()) {
+                                Log.i(TAG, "Purchasing " + purchase.getSku() +
+                                        " is Successful, let's consume it now");
+                                consumeIAP(purchase);
+                            }
+                        }
+                    }, developerPayload);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            Log.e(TAG, "Purchase Exception", e);
+        }
+    }
+
+    protected void handleInAppPurchase(BuyableType buyableType) {
+        this.handleInAppPurchase(buyableType.getSku(), buyableType.getRequestCode(),
+                buyableType.getDeveloperPayload());
+    }
+
+    public void loadMyInventory() {
+        try {
+            iabHelper.queryInventoryAsync(new IabHelper.QueryInventoryFinishedListener() {
+                @Override
+                public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+                    if (result.isSuccess()) {
+                        for (String consumable : consumables) {
+                            if (inv.hasPurchase(consumable)) {
+                                Log.i(TAG, "Consumable " + consumable +
+                                        " is with me. Let's consume it now.");
+                                consumeIAP(inv.getPurchase(consumable));
+                            }
+                        }
+                    }
+                }
+            });
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            Log.e(TAG, "Exception in querying my inventory", e);
+        }
+    }
+
+    protected void consumeIAP(Purchase consumable) {
+        try {
+            iabHelper.consumeAsync(consumable, new IabHelper.OnConsumeFinishedListener() {
+                @Override
+                public void onConsumeFinished(Purchase purchase, IabResult result) {
+                    if (result.isSuccess()) {
+                        Log.i(TAG, "Consumption of " + purchase.getSku() +
+                                " is Successful, let's update the db.");
+                        IAPCreditor iapCreditor = iapCreditorFactory
+                                .getIAPCreditor(purchase.getSku(), getApplicationContext());
+                        iapCreditor.creditItems();
+                        onConsumptionComplete();
+                    }
+                }
+            });
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            Log.e(TAG, "Exception in consuming purchase. Lulz!", e);
+        }
+    }
+
+    protected abstract void onConsumptionComplete();
+}
--- app/src/main/java/com/asb/goldtrap/ShopORamaActivity.java
+++ app/src/main/java/com/asb/goldtrap/ShopORamaActivity.java
@@ -1,46 +1,27 @@
 package com.asb.goldtrap;
 
-import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.asb.goldtrap.fragments.shoporama.CheckoutFragment;
 import com.asb.goldtrap.fragments.shoporama.ShopORamaFragment;
-import com.asb.goldtrap.iap.util.IabBroadcastReceiver;
-import com.asb.goldtrap.iap.util.IabHelper;
-import com.asb.goldtrap.iap.util.IabResult;
-import com.asb.goldtrap.iap.util.Inventory;
-import com.asb.goldtrap.iap.util.Key;
-import com.asb.goldtrap.iap.util.Purchase;
 import com.asb.goldtrap.models.buyables.BuyableType;
 import com.asb.goldtrap.models.eo.Buyable;
-import com.asb.goldtrap.models.iap.IAPCreditor;
-import com.asb.goldtrap.models.iap.factory.IAPCreditorFactory;
 
-public class ShopORamaActivity extends AppCompatActivity
-        implements IabBroadcastReceiver.IabBroadcastListener,
-        ShopORamaFragment.OnFragmentInteractionListener,
+public class ShopORamaActivity extends AbstractPurchaseActivity
+        implements ShopORamaFragment.OnFragmentInteractionListener,
         CheckoutFragment.OnFragmentInteractionListener {
 
     public static final String BOOSTER_TYPE = "BOOSTER_TYPE";
     private static final String TAG = ShopORamaActivity.class.getSimpleName();
-    private IabHelper iabHelper;
-    private IabBroadcastReceiver mBroadcastReceiver;
-    private IAPCreditorFactory iapCreditorFactory;
-    private String[] consumables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_orama);
-        iapCreditorFactory = new IAPCreditorFactory();
-        consumables = getResources().getStringArray(R.array.iap_sku);
-        setupIAP();
         if (null != getIntent().getExtras()) {
             String boosterType = getIntent().getExtras().getString(BOOSTER_TYPE);
             getSupportFragmentManager().beginTransaction()
@@ -59,34 +40,6 @@ public class ShopORamaActivity extends AppCompatActivity
                             ShopORamaFragment.TAG)
                     .commit();
         }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mBroadcastReceiver != null) {
-            unregisterReceiver(mBroadcastReceiver);
-        }
-        Log.d(TAG, "Destroying helper.");
-        if (iabHelper != null) {
-            iabHelper.disposeWhenFinished();
-            iabHelper = null;
-        }
-    }
-
-    private void setupIAP() {
-        iabHelper = new IabHelper(this, Key.value());
-        iabHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-            public void onIabSetupFinished(IabResult result) {
-                Log.d(TAG, "Setup finished.");
-                if (null != iabHelper && result.isSuccess()) {
-                    mBroadcastReceiver = new IabBroadcastReceiver(ShopORamaActivity.this);
-                    IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
-                    registerReceiver(mBroadcastReceiver, broadcastFilter);
-                    loadMyInventory();
-                }
-            }
-        });
     }
 
     @Override
@@ -140,68 +93,9 @@ public class ShopORamaActivity extends AppCompatActivity
         handleInAppPurchase(buyableType);
     }
 
-    private void handleInAppPurchase(BuyableType buyableType) {
-        try {
-            if (null != iabHelper) iabHelper.flagEndAsync();
-            iabHelper.launchPurchaseFlow(this, buyableType.getSku(), buyableType.getRequestCode(),
-                    new IabHelper.OnIabPurchaseFinishedListener() {
-                        @Override
-                        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-                            if (result.isSuccess()) {
-                                Log.i(TAG, "Purchasing " + purchase.getSku() +
-                                        " is Successful, let's consume it now");
-                                consumeIAP(purchase);
-                            }
-                        }
-                    }, buyableType.getDeveloperPayload());
-        } catch (IabHelper.IabAsyncInProgressException e) {
-            Log.e(TAG, "Purchase Exception", e);
-        }
-    }
-
-    public void loadMyInventory() {
-        try {
-            iabHelper.queryInventoryAsync(new IabHelper.QueryInventoryFinishedListener() {
-                @Override
-                public void onQueryInventoryFinished(IabResult result, Inventory inv) {
-                    if (result.isSuccess()) {
-                        for (String consumable : consumables) {
-                            if (inv.hasPurchase(consumable)) {
-                                Log.i(TAG, "Consumable " + consumable +
-                                        " is with me. Let's consume it now.");
-                                consumeIAP(inv.getPurchase(consumable));
-                            }
-                        }
-                    }
-                }
-            });
-        } catch (IabHelper.IabAsyncInProgressException e) {
-            Log.e(TAG, "Exception in querying my inventory", e);
-        }
-    }
-
-    private void consumeIAP(Purchase consumable) {
-        try {
-            iabHelper.consumeAsync(consumable, new IabHelper.OnConsumeFinishedListener() {
-                @Override
-                public void onConsumeFinished(Purchase purchase, IabResult result) {
-                    if (result.isSuccess()) {
-                        Log.i(TAG, "Consumption of " + purchase.getSku() +
-                                " is Successful, let's update the db.");
-                        IAPCreditor iapCreditor = iapCreditorFactory
-                                .getIAPCreditor(purchase.getSku(), getApplicationContext());
-                        iapCreditor.creditItems();
-                        showMessage(getString(R.string.transaction_successful));
-                    }
-                }
-            });
-        } catch (IabHelper.IabAsyncInProgressException e) {
-            Log.e(TAG, "Exception in consuming purchase. Lulz!", e);
-        }
-    }
-
-    private void showMessage(String msg) {
+    protected void onConsumptionComplete() {
         ViewGroup coordinateLayout = (ViewGroup) findViewById(R.id.fragment_container);
-        Snackbar.make(coordinateLayout, msg, Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(coordinateLayout, getString(R.string.transaction_successful),
+                Snackbar.LENGTH_SHORT).show();
     }
 }
