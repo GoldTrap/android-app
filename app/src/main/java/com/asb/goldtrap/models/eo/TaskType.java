2f1f6e5824f8e97a0ce0fd68fbad1322065f31c8
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-07-17 13:44:04 +0530

    Update Task Types

--- app/src/main/java/com/asb/goldtrap/models/eo/TaskType.java
+++ app/src/main/java/com/asb/goldtrap/models/eo/TaskType.java
@@ -6,14 +6,14 @@ import com.asb.goldtrap.R;
  * Created by arjun on 07/11/15.
  */
 public enum TaskType {
-    POINTS(R.drawable.coins, R.plurals.get_points),
-    LINES(R.drawable.spark, R.plurals.get_lines),
-    HORIZONTAL_LINE(R.drawable.spark, R.plurals.get_horizontal_lines),
-    VERTICAL_LINE(R.drawable.spark, R.plurals.get_vertical_lines),
-    GOODIES(R.drawable.coins, R.plurals.get_goodies),
+    POINTS(R.drawable.empty_star, R.plurals.get_points),
+    LINES(R.drawable.filled_star, R.plurals.get_lines),
+    HORIZONTAL_LINE(R.drawable.filled_star, R.plurals.get_horizontal_lines),
+    VERTICAL_LINE(R.drawable.filled_star, R.plurals.get_vertical_lines),
+    GOODIES(R.drawable.spark, R.plurals.get_goodies),
     ONE_K(R.drawable.coins, R.plurals.get_golds),
-    TWO_K(R.drawable.coins, R.plurals.get_golds),
-    FIVE_K(R.drawable.coins, R.plurals.get_golds),
+    TWO_K(R.drawable.gold_nuggets, R.plurals.get_gold_nuggets),
+    FIVE_K(R.drawable.gold_bars, R.plurals.get_gold_bars),
     DIAMOND(R.drawable.diamond, R.plurals.get_diamonds),
     DYNAMIC_GOODIE(R.drawable.spark, R.plurals.get_dynamic_goodies);
 
