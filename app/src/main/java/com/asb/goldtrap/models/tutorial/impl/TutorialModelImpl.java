8b91213f6dc2fe69a1657ddc44e0c41b7342ba21
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2016-08-13 14:25:11 +0530

    Update Tutorial to use Primary Light color

--- app/src/main/java/com/asb/goldtrap/models/tutorial/impl/TutorialModelImpl.java
+++ app/src/main/java/com/asb/goldtrap/models/tutorial/impl/TutorialModelImpl.java
@@ -65,30 +65,30 @@ public class TutorialModelImpl implements TutorialModel {
     private TutorialItem clickSkip(Context context) {
         return new TutorialItem(context.getString(R.string.skip_title),
                 context.getString(R.string.skip_subtitle),
-                R.color.md_blue_A200, R.drawable.skip_tut);
+                R.color.primary_light, R.drawable.skip_tut);
     }
 
     private TutorialItem clickPlusOne(Context context) {
         return new TutorialItem(context.getString(R.string.plus_one_title),
                 context.getString(R.string.plus_one_subtitle),
-                R.color.md_blue_A200, R.drawable.plus_one_tut);
+                R.color.primary_light, R.drawable.plus_one_tut);
     }
 
     private TutorialItem clickFlip(Context context) {
         return new TutorialItem(context.getString(R.string.flip_title),
                 context.getString(R.string.flip_subtitle),
-                R.color.md_blue_A200, R.drawable.flip_tut);
+                R.color.primary_light, R.drawable.flip_tut);
     }
 
     private TutorialItem getPoints(Context context) {
         return new TutorialItem(context.getString(R.string.points_title),
                 context.getString(R.string.points_subtitle),
-                R.color.md_blue_A200, R.drawable.point_tut);
+                R.color.primary_light, R.drawable.point_tut);
     }
 
     private TutorialItem getClick(Context context) {
         return new TutorialItem(context.getString(R.string.click_title),
                 context.getString(R.string.click_subtitle),
-                R.color.md_blue_A200, R.drawable.click_tut);
+                R.color.primary_light, R.drawable.click_tut);
     }
 }
